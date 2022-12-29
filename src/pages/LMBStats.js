92adbbbc182a9b4496ac976350cdd67b5bd2c2e0
Diego <diegogplata@gmail.com>
2022-12-29 12:28:13 -0600

    introduce first two charts with LiMB stats. HR and AVG charts

--- src/pages/LMBStats.js
+++ src/pages/LMBStats.js
@@ -1,17 +1,26 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import lmb_stats from '../assets/top_25_at_bat_LMB.json'
 
 function LMBStats() {
-    const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},
-        {name: 'Page B', uv: 800, pv: 1400, amt: 1400},
-    {name: 'Page C', uv: 650, pv: 2400, amt: 2400}]
   return (
     <div className="">
-        <LineChart width={600} height={300} data={data}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <LineChart width={600} height={300} data={lmb_stats}>
+            <Line type="monotone"
+                  dataKey="HR"
+                  stroke="#8884d8" />
             <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="name" />
+            <XAxis dataKey="player" />
             <YAxis />
+            <Tooltip />
+        </LineChart>
+        <LineChart width={600} height={300} data={lmb_stats}>
+            <Line type="monotone"
+                  dataKey="AVG"
+                  stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="player" />
+            <YAxis />
+            <Tooltip />
         </LineChart>
     </div>
   )
