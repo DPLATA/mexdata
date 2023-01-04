a2de6fd41745b772a99403d3edc779936bc1e717
Diego <diegogplata@gmail.com>
2023-01-03 21:49:30 -0600

    populate charts from API data

--- src/pages/LMBStatsPage.js
+++ src/pages/LMBStatsPage.js
@@ -1,8 +1,33 @@
 import CustomLineChart from '../components/Charts/LineChart/CustomLineChart'
-import lmb_stats from '../assets/top_25_at_bat_LMB.json'
+// import lmb_stats from '../assets/top_25_at_bat_LMB.json'
+import { useState, useEffect } from "react"
 
 
 function LMBStatsPage() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                let response = await fetch('https://mexdata-api.onrender.com/lmb/players_hitting')
+                let actualData = await response.json()
+                // console.log(actualData.data)
+                setData(actualData.data)
+                setError(null)
+            }catch (error){
+                // console.log(error.message)
+                setError(error.message)
+                setData(null)
+            }finally{
+                setLoading(false)
+            }
+        }
+        getData()
+        console.log(data)
+    }, [])
+
     return (
         <>
             <section className="section">
@@ -11,16 +36,16 @@ function LMBStatsPage() {
             <section className="section">
                 <div className="container">
                     <div className="columns is-multiline">
-                        <CustomLineChart width={600} height={300} data={lmb_stats} name='home runs'
+                        <CustomLineChart width={600} height={300} data={data} name='home runs'
                                          xAxisDataKey='player' type='monotone' gridHexColor='#ccc' legendHeight={36}
                                          lineDataKey='HR' strokeHexColor='#8884d8'/>
-                        <CustomLineChart width={600} height={300} data={lmb_stats} name='porcentaje de bateo'
+                        <CustomLineChart width={600} height={300} data={data} name='porcentaje de bateo'
                                          xAxisDataKey='player' type='monotone' gridHexColor='#ccc' legendHeight={36}
                                          lineDataKey='AVG' strokeHexColor='#8884d8'/>
-                        <CustomLineChart width={600} height={300} data={lmb_stats} name='carreras impulsadas'
+                        <CustomLineChart width={600} height={300} data={data} name='carreras impulsadas'
                                          xAxisDataKey='player' type='monotone' gridHexColor='#ccc' legendHeight={36}
                                          lineDataKey='RBI' strokeHexColor='#8884d8'/>
-                        <CustomLineChart width={600} height={300} data={lmb_stats} name='carreras'
+                        <CustomLineChart width={600} height={300} data={data} name='carreras'
                                          xAxisDataKey='player' type='monotone' gridHexColor='#ccc' legendHeight={36}
                                          lineDataKey='R' strokeHexColor='#658354'/>
                     </div>
