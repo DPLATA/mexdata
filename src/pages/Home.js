e5a2c295f1146bac24ee147bf9caff42820ed8d1
Diego <diegogplata@gmail.com>
2023-01-05 21:20:20 -0600

    update home screen styling, will use bulma library to add navbar and hero blank section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,25 @@ import { Link } from 'react-router-dom'
 export const Home = () => {
     return (
     <>
-        <Link className="link" to='/lmbstats'>Estadísticas LMB</Link>
-        <Link className="link" to='/map'>Mapa con información del INEGI</Link>
-        <a className="link" href="https://medium.com/moneyball-en-español" target="_blank" rel="noreferrer">Moneyball en español</a>
-        {/*deportes - triatlon - fba*/}
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-menu">
+                <div className="navbar-end">
+                    <Link className="navbar-item" to='/lmbstats'>Estadísticas LMB</Link>
+                    <Link className="navbar-item " to='/map'>Mapa con información del INEGI</Link>
+                    <a className="navbar-item" href="https://medium.com/moneyball-en-español" target="_blank" rel="noreferrer">Moneyball en español</a>
+                </div>
+            </div>
+        </nav>
+        <section className="hero is-fullheight-with-navbar is-info">
+            <div className="hero-body">
+            </div>
+        </section>
+
+
+        {/*deportes - triatlon - fba
+            Meterle agregador de noticias y estilos para v1
+            Meterle seccion de musica y radio
+        */}
     </>
   )
 }
