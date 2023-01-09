47fb84c14397bfbef105f54fb7a86f4e614407f3
Diego <diegogplata@gmail.com>
2023-01-09 14:48:18 -0600

    add news list section to home page, removes hero section, styling is WIP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,33 @@
 import './Home.css'
 import { Link } from 'react-router-dom'
+import {useEffect, useState} from "react";
+
+
+
 
 export const Home = () => {
+
+
+    let [data, setData] = useState([]);
+    let [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                let response = await fetch('https://mexdata-api.onrender.com/lmb/news_headers')
+                let actualData = await response.json()
+                setData(actualData.data)
+            } catch (error) {
+                setData(null)
+                console.log(error.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        getData()
+    }, [])
+
+
     return (
     <>
         <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -13,15 +39,38 @@ export const Home = () => {
                 </div>
             </div>
         </nav>
-        <section className="hero is-fullheight-with-navbar is-info">
-            <div className="hero-body">
-            </div>
+        <section className=" is-fullheight-with-navbar">
+            {//<div className="hero-body">
+                 }
+                <div className="list">
+                { data.map(header => {
+                        return (<div className="list-item card" key={header.id}>
+                            <div className="card-content" >
+                                <p className="content has-text-weight-semibold" >
+                                    {header.title}
+                                </p>
+                        </div>
+                        </div>)
+                }) }
+                </div>
+            { //</div>
+                }
         </section>
-
+        {/* News aggregator for home screen
+            poll f polls section
+            presidenciables news section
+            poll aggregator
+        */}
 
         {/*deportes - triatlon - fba
             Meterle agregador de noticias y estilos para v1
             Meterle seccion de musica y radio
+            apartado especial el cuauh por la combinacion perfecta entre deporte y politica
+            eñ sitio de noticias mas gracioso de mexico
+
+            Mapas - CDMX shapefiles info datos abiertos gob cdmx
+            MAPAS - cuantos estados son gobernados por hombres y cauntos por mujeres
+            Seccion acerca de
         */}
     </>
   )
