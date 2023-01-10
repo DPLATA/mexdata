3f0beec0ce3712d4d50acd4e0076cdf71f8b77ee
Diego <diegogplata@gmail.com>
2023-01-10 13:17:07 -0600

    introduce changes for responsive burger menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,8 @@ export const Home = () => {
 
 
     let [data, setData] = useState([]);
+    let [isActive, setIsActive] = useState(false);
+
 
     useEffect(() => {
         const getData = async () => {
@@ -30,7 +32,23 @@ export const Home = () => {
     return (
     <>
         <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-menu">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img alt="logo" src="https://bulma.io/images/placeholders/128x128.png" width="32" height="32"/>
+                </a>
+
+                <button onClick={() => {
+                    setIsActive(!isActive);
+                }} className={`navbar-burger ${isActive ? "is-active" : ""}`} aria-label="menu" aria-expanded="false"
+                   data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </button>
+            </div>
+            <div id="navbarBasicExample"
+                 className={`navbar-menu ${isActive ? "is-active" : ""}`}
+            >
                 <div className="navbar-end">
                     <Link className="navbar-item" to='/lmbstats'>Estadísticas LMB</Link>
                     <Link className="navbar-item " to='/map'>Mapa con información del INEGI</Link>
