635c76d90ca896e5c55d130e6cc9964f4ade6a21
Diego <diegogplata@gmail.com>
2023-01-28 10:11:39 -0600

    update navbar copy and links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,7 +9,7 @@ export const Navigation = () => {
     <>
         <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
-                <a className="navbar-item" href="https://relaxed-zuccutto-a8320d.netlify.app">
+                <a className="navbar-item" href="https://eldatomx.com">
                     <img alt="logo" src="https://bulma.io/images/placeholders/128x128.png" width="32" height="32"/>
                 </a>
 
@@ -26,10 +26,10 @@ export const Navigation = () => {
                  className={`navbar-menu ${isActive ? "is-active" : ""}`}
             >
                 <div className="navbar-end">
-                    <Link className="navbar-item" to='/'>Feed de inicio</Link>
+                    <Link className="navbar-item" to='/'>Inicio</Link>
                     <Link className="navbar-item" to='/lmbstats'>Estadísticas LMB</Link>
                     {/*Estadisticas LFA <Link className="navbar-item" to='/lmbstats'>Estadísticas LMB</Link>*/}
-                    <Link className="navbar-item " to='/map'>Mapa con información del INEGI</Link>
+                    <Link className="navbar-item " to='/map'>Mapa interactivo INEGI</Link>
                     <Link className="navbar-item " to='/political_map'>Mapa político de México</Link>
                     {/* CDMX standalone map <Link className="navbar-item " to='/map'>Mapa con información del INEGI</Link>*/}
                     {/*Galeria de mapas demograficos rendereados con r<Link className="navbar-item " to='/map'>Mapa con información del INEGI</Link>*/}
